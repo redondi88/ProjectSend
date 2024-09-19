28d2931cd68c366436f3bf390010f96fdb4e6f8a
Diego <diego@redondo.ca>
2024-09-19 02:23:06 -0700

    Refactor Security

--- includes/security/plugins.php
+++ includes/security/plugins.php
@@ -1,33 +1,50 @@
 <?php
-// error_reporting(E_ALL);
+
 // Get the request URI
 $path = $_SERVER['REQUEST_URI'];
 
+/**
+ * Check if the path contains '/plugins/'.
+ * 
+ * @param string $path
+ * @return bool
+ */
 function isPlugin($path)
 {
-    // Check if '/plugins/' is in the path
-    if (strpos($path, '/plugins/') !== false) {
-        return true;
-    }
-
-    return false;
+    return strpos($path, '/plugins/') !== false;
 }
+
+/**
+ * Check if the plugin corresponding to the given path is enabled.
+ * 
+ * @param string $path
+ * @return bool
+ */
 function isPluginEnabled($path)
 {
     global $dbh;
-    $sql = "SELECT * FROM " . TABLE_PLUGINS . " WHERE enabled = TRUE";
+
+    // Prepare and execute the SQL query to check for enabled plugins with the specified path
+    $sql = "SELECT path FROM " . TABLE_PLUGINS . " WHERE enabled = TRUE";
     $statement = $dbh->prepare($sql);
     $statement->execute();
+    
+    // Fetch all enabled plugin paths
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // Check if the requested path matches any enabled plugin's path
     foreach ($results as $row) {
-        if (strpos($path, $row["path"]) !== true) {
+        if (strpos($path, $row['path']) === false) {
             return true;
         }
     }
+    // die();
     return false;
 }
+
+// Check if the current request is for a plugin
 if (isPlugin($path)) {
-    // find plugin and check if enabled 
+    // If the plugin is not enabled, return a 404 error
     if (!isPluginEnabled($path)) {
         exit_with_error_code(404);
     }
